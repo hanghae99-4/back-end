1ed49c57ab458ca44b3b801bceb59e824b818c14
ryucm <ckd12394@gmail.com>
2022-10-29 16:07:42 +0900

    Add Swagger in MemberControll

--- instakilogram/src/main/java/com/hanghae/instakilogram/controller/MemberController.java
+++ instakilogram/src/main/java/com/hanghae/instakilogram/controller/MemberController.java
@@ -5,12 +5,15 @@ import com.hanghae.instakilogram.dto.request.*;
 import com.hanghae.instakilogram.dto.response.ResponseDto;
 import com.hanghae.instakilogram.security.UserDetailsImpl;
 import com.hanghae.instakilogram.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
+@Tag(name = "Member", description = "회원관리 API")
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
@@ -18,21 +21,25 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
+    @Operation(summary = "Sign Up", description = "회원가입")
     public ResponseDto<?> signUp(@RequestBody SignupRequestDto signupRequestDto) {
         return memberService.signUp(signupRequestDto);
     }
 
     @PostMapping("/login")
+    @Operation(summary = "Log In", description = "로그인")
     public ResponseDto<?> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse httpServletResponse) {
         return memberService.login(loginRequestDto, httpServletResponse);
     }
 
     @GetMapping("/signup/checkid")
+    @Operation(summary = "ID Check", description = "멤버 ID 확인")
     public ResponseDto<?> checkId(@RequestBody CheckIdRequestDto checkIdRequestDto) {
         return memberService.checkId(checkIdRequestDto);
     }
 
     @GetMapping("/signup/checknickname")
+    @Operation(summary = "Nickname Check", description = "멤버 Nickname 확인")
     public ResponseDto<?> checkNickname(@RequestBody CheckNicknameRequestDto checkNicknameRequestDto) {
         return memberService.checkNickname(checkNicknameRequestDto);
     }
---
+++ instakilogram/src/main/java/com/hanghae/instakilogram/security/config/Swagger2Config.java
@@ -0,0 +1,26 @@
+package com.hanghae.instakilogram.security.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Swagger2Config {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("instakilogram")
+                .pathsToMatch("/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Instakilogram API")
+                        .description("인스타그램을 재해석한 차세대 SNS입니다.")
+                        .version("v0.0.1"));
+    }
+}
